431fd061e71754a1863ed6630deb59469049208d
FilopateerFouad <99017591+FilopateerFouad@users.noreply.github.com>
2024-04-03 19:40:14 +0200

    Create my first flutter project

---
+++ company business application/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/myproject/welcome.dart';
+main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+      home:Welcome1())
+  );
+}
